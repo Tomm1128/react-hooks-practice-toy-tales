7cbe8a0fb3a89066079b327706fc22b5a87b7202
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-06-24 14:49:10 -0400

    create createToy function

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,15 @@ const getToys = () => {
   return fetch(api).then((resp) => resp.json())
 }
 
-export { getToys }
+const createToy = (toy) => {
+  return fetch(api, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(toy),
+  }).then((resp) => resp.json())
+}
+
+export { getToys, createToy }
